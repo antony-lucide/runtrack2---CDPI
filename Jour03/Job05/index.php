b1384a7f1e498245a8dd95f1d7783deabdb6dd37
AntonyL-prog <lucideantony@gmail.com>
2024-05-25 18:09:27 +0200

    Ceci est le rendu final du jour03 de la runtrack CDPI PHP

--- Jour03/Job05/index.php
+++ Jour03/Job05/index.php
@@ -3,31 +3,54 @@
 $str = "On n’est pas le meilleur quand on le croit mais quand on le sait";
 $voyelles = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'];
 
-$dic = {"Consonnes" => 0,
-        "Voyelles" => 0;
-       };
+$dic = ["Consonnes" => 0,
+        "Voyelles" => 0,
+       ];
 
-$count = 0;
 $isvoyelle = false;
 $isconsonne = false;
 
-for ($i = 0; $i < $str[$i]; $i++) {
-   $count++;
-}
+for ($i = 0;  isset($str[$i]); $i++) { #Travel through the str
+    // var_dump($str[$i]);
+    if($str[$i] >= "A" && $str[$i] <= "z" || $str[$i] >= "A" && $str[$i] <= "Z"){
+        for($j = 0; isset($voyelles[$j]); $j++) #Verify Voyelle
+        // var_dump($voyelles[$j]);
+            if($str[$i] == $voyelles[$j]){
+                $isvoyelle = true;
+                break;
+            }
+    }
 
-for($j = 0; isset($voyelles[$j]); $j++){
-    if($str[$count] == $voyelles[$j]){
-        $isvoyelle = true;
-        break;
+    if($isvoyelle){
+        $dic["Voyelles"]++;
+    } else{
+        $dic["Consonnes"]++;
     }
 }
+
+
 ?>
 
 
-<?php 
-foreach ($dic as $keys => $value){
-    echo "<tr>
-            <td>$key</td>
-            <td>$value[$count]</td> 
-          </tr>";
-}?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Count Vowels and Consonants</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <th>Type</th>
+            <th>Count</th>
+        </tr>
+        <?php foreach ($dic as $key => $value) { ?>
+            <tr>
+                <td><?php echo $key; ?></td>
+                <td><?php echo $value; ?></td>
+            </tr>
+        <?php } ?>
+    </table>
+</body>
+</html>
